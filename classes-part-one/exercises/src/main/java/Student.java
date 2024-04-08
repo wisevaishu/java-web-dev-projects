1c86edd676000d436513b10ef43eb55ff7a3c5e1
wisevaishu <vpm.win@gmail.com>
2024-04-08 16:02:11 -0500

    class part one exercise completed

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -4,7 +4,47 @@ public class Student {
         private int numberOfCredits = 0;
         private double gpa = 0.0;
 
+        public Student(String stuName, int stuID, int noOfCredits, double GPA) {
+                this.name=stuName;
+                this.studentId=stuID;
+                this.numberOfCredits=noOfCredits;
+                this.gpa=GPA;
+        }
+
         // Drop your getters and setters below for the Student class.
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public void setStudentId(int studentId) {
+                this.studentId = studentId;
+        }
+
+        public void setGpa(double gpa) {
+                this.gpa = gpa;
+        }
+
+        private void setNumberOfCredits(int numberOfCredits) {
+                this.numberOfCredits = numberOfCredits;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public int getStudentId() {
+                return studentId;
+        }
+
+        public int getNumberOfCredits() {
+                return numberOfCredits;
+        }
+
+        public double getGpa() {
+                return gpa;
+        }
+
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
 }
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,6 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student student = new Student("Mohnish", 12345, 1, 4.0);
     }
 }
